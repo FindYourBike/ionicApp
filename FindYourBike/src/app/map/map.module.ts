b4221d1fe2d218d175ab09eb74ff6ca0b793f604
Robbe Willeme <robbe.willeme@hotmail.com>
2021-03-23 16:23:48 +0100

    Added google maps api

--- FindYourBike/src/app/map/map.module.ts
+++ FindYourBike/src/app/map/map.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,10 @@ import { MapPage } from './map.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    MapPageRoutingModule
+    MapPageRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB1P2R6rG3UA68hCKvBS3nY-YMPzSkore8'
+    })
   ],
   declarations: [MapPage]
 })
--- FindYourBike/src/app/map/map.page.ts
+++ FindYourBike/src/app/map/map.page.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  latitude = 51.230077585660105;
+  longitude = 4.416849648098205;
 
-  constructor() { }
+  lat = 52.230077585660105;
+  long = 4.416849648098205;
+
+  gemiddeldeLat: number;
+  gemiddeldeLong: number;
+
+  constructor() { 
+    this.gemiddeldeLat = (this.latitude + this.lat)/2;
+    this.gemiddeldeLong = (this.longitude + this.long)/2;
+  }
 
   ngOnInit() {
   }
