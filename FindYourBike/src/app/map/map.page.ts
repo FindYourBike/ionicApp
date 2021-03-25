f06776149289f2b544f537a7dc9a8b73535e77bb
Robin <Robinsegers1@gmail.com>
2021-03-25 18:25:44 +0100

    Get location from API

--- FindYourBike/src/app/map/map.page.ts
+++ FindYourBike/src/app/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService, IBikeInfo } from '../services/api.service';
 
 @Component({
   selector: 'app-map',
@@ -6,18 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  latitude = 51.230077585660105;
-  longitude = 4.416849648098205;
+  
+  // latitude = 51.230077585660105;
+  // longitude = 4.416849648098205;
+
+  latitude: string;
+  longitude: string;
+
 
   //gemiddeldeLat: number;
   //gemiddeldeLong: number;
 
-  constructor() { 
+  constructor(public service : APIService) { 
     //this.gemiddeldeLat = (this.latitude + this.lat)/2;
     //this.gemiddeldeLong = (this.longitude + this.long)/2;
+
+    this.service.GetBikeInfo().subscribe(response => this.SetBikeInfo(response))
+
   }
 
   ngOnInit() {
+  }
+
+  SetBikeInfo(info : IBikeInfo){
+    console.log(info);
+    this.latitude = info.latitude;
+    this.longitude = info.lontitude;
   }
 
 }
---
+++ FindYourBike/src/app/services/api.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  constructor(private http : HttpClient) { }
+
+  GetBikeInfo(){
+    return this.http.get<IBikeInfo>(`https://xdzj4zber6.execute-api.us-east-1.amazonaws.com/dev/bikes/TestDemo1`)
+  }
+}
+
+
+
+  export interface IBikeInfo {
+      lontitude: string;
+      BikeID: string;
+      latitude: string;
+      LocationTimeStamp: number;
+  }
+
+
+
+
