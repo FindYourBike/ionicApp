878ccd131692e126e1bf5d228fd2b7552e03fb46
Robin <Robinsegers1@gmail.com>
2021-03-25 20:12:24 +0100

    Revert "Get location from API"
    This reverts commit f06776149289f2b544f537a7dc9a8b73535e77bb.

--- FindYourBike/src/app/map/map.page.ts
+++ FindYourBike/src/app/map/map.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { APIService, IBikeInfo } from '../services/api.service';
 
 @Component({
   selector: 'app-map',
@@ -7,32 +6,18 @@ import { APIService, IBikeInfo } from '../services/api.service';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  
-  // latitude = 51.230077585660105;
-  // longitude = 4.416849648098205;
-
-  latitude: string;
-  longitude: string;
-
+  latitude = 51.230077585660105;
+  longitude = 4.416849648098205;
 
   //gemiddeldeLat: number;
   //gemiddeldeLong: number;
 
-  constructor(public service : APIService) { 
+  constructor() { 
     //this.gemiddeldeLat = (this.latitude + this.lat)/2;
     //this.gemiddeldeLong = (this.longitude + this.long)/2;
-
-    this.service.GetBikeInfo().subscribe(response => this.SetBikeInfo(response))
-
   }
 
   ngOnInit() {
-  }
-
-  SetBikeInfo(info : IBikeInfo){
-    console.log(info);
-    this.latitude = info.latitude;
-    this.longitude = info.lontitude;
   }
 
 }
