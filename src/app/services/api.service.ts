48dccc61c667da8733dbe2d5292a9a7befcee901
Luca De Lauw <lucadelauw@gmail.com>
2021-03-27 19:33:27 +0100

    "Last time updated" formatting

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -22,7 +22,13 @@ export class APIService {
   }
 
   GetBikes(){
-    return this.http.get<IBikes>(`https://xdzj4zber6.execute-api.us-east-1.amazonaws.com/dev/users/` + this.service.getUserID())
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/xml',
+        'Authorization': this.service.getToken()
+      })
+    };
+    return this.http.get<IBikes>(`https://xdzj4zber6.execute-api.us-east-1.amazonaws.com/dev/users/` + this.service.getUserID(), httpOptions)
   }
 }
 
