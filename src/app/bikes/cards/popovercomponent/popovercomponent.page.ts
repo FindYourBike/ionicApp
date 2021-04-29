6a30a0a1916ff68b7c38a8955b0e6a4b6818fd64
Luca De Lauw <lucadelauw@gmail.com>
2021-04-29 12:39:44 +0200

    change bikename

--- src/app/bikes/cards/popovercomponent/popovercomponent.page.ts
+++ src/app/bikes/cards/popovercomponent/popovercomponent.page.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { PopoverController } from '@ionic/angular';
+import { AlertController, PopoverController } from '@ionic/angular';
 import { APIService, IBikePing } from 'src/app/services/api.service';
 
 @Component({
@@ -13,7 +13,7 @@ export class PopovercomponentPage implements OnInit {
 
   @Input() public bike: IBikePing;
   
-  constructor(private popover:PopoverController, public service : APIService, private router: Router) { }
+  constructor(private popover:PopoverController, public service : APIService, private router: Router, public alertController: AlertController) { }
 
   ngOnInit() {
     console.log(this.bike)
@@ -46,6 +46,60 @@ export class PopovercomponentPage implements OnInit {
 
     })
     this.router.navigate(['/tabs/bikes']);
+  }
+
+  async tryChangeBikeName(newname: string) {
+    var body = {}
+    var bikelist = new Array()
+    this.service.GetBikes().subscribe(bikes => {
+      if(bikes.bikes != undefined || bikes.bikes != null) {
+        bikes.bikes.forEach(element => {
+          bikelist.push(element)
+        });
+      }
+      console.log(this.bike)
+      for (var i = 0; i < bikelist.length; i ++) {
+        if (bikelist[i].bikeid == this.bike.id) {
+          bikelist[i].bikename = newname
+          console.log("match")
+        }
+      }
+      console.log(bikelist)
+      body["bikes"] = bikelist
+      this.service.PatchUser(JSON.stringify(body))
+    })
+    this.router.navigate(['/tabs/bikes']);
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Change name',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'name'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Confirm',
+          handler: (data: any) => {
+            console.log(data);
+            this.tryChangeBikeName(data.name)
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
